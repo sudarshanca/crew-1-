f11b5c67a31fe864205c5f2b196f16e9c721bb3e
sudarshanca <sudarshanreddyca@gmail.com>
2024-06-10 11:58:02 +0530

    10 v-2

--- dashBoard/dashboard.js
+++ dashBoard/dashboard.js
@@ -1,16 +1,40 @@
 $(function () {
-  $("#listing-details-photo").load("listing-details-photo.html");
-});
+  $("#dashboardHeader").load("dashboardHeader.html", function() {
+  });
 
+  $("#listing-details-photo").load("listing-details-photo.html", function() {
+    // Initialize Scrollspy after content is loaded
+    const scrollElement1 = document.querySelector('#ListingDetails1');
+    const scrollSpy1 = new bootstrap.ScrollSpy(scrollElement1, {
+      target: '#page1',
+      smoothScroll: true 
+    });
+  });
+  
+  $("#pricingapi1").load("pricingapi1.html", function() {
+    // Initialize Scrollspy after content is loaded
+    const scrollElement2 = document.querySelector('#PricingApi');
+    const scrollSpy2 = new bootstrap.ScrollSpy(scrollElement2, {
+      target: '#page2',
+      smoothScroll: true 
+    });
+  });
 
-$(function () {
-  $("#pricing").load("pricing.html");
-});
+  $("#rulesAndPolices").load("rulesAndPolices.html", function() {
+    // Initialize Scrollspy after content is loaded
+    const scrollElement2 = document.querySelector('#PricingApi');
+    const scrollSpy2 = new bootstrap.ScrollSpy(scrollElement2, {
+      target: '#page2',
+      smoothScroll: true 
+    });
+  });
 
-$(function () {
-  $("#pricingapi1").load("pricingapi1.html");
-});
-
-$(function () {
-  $("#rulesAndPolices1").load("rulesAndPolices1.html");
+  $("#calander-availability").load("calander-availability.html", function() {
+    // Initialize Scrollspy after content is loaded
+    const scrollElement2 = document.querySelector('#PricingApi');
+    const scrollSpy2 = new bootstrap.ScrollSpy(scrollElement2, {
+      target: '#page2',
+      smoothScroll: true 
+    });
+  });
 });
